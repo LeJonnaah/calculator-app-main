b9b2852cc91556ec7fe8cbfcc9b4162b30bed6d4
Jandueza <jonathanandueza1201@gmail.com>
2022-12-01 12:21:52 -0300

    Challenge Completed

--- js/main.js
+++ js/main.js
@@ -4,53 +4,55 @@
 
 const calculator = document.querySelector('.calculator')
 const button = document.querySelector('.calculator__button')
-const display = document.querySelector('.calculator__display')
+const displayContent = document.querySelector('.calculator__display-text')
 
 // Functions
 
-function calculate (firstNumber, operator, secondNumber) {
-    firstNumber = parseFloat(firstNumber)
-    secondNumber = parseFloat(secondNumber)
-
-    if (operator === 'add') return firstNumber + secondNumber
-    if (operator === 'subtract') return firstNumber - secondNumber
-    if (operator === 'multiply') return firstNumber * secondNumber
-    if (operator === 'divide') return firstNumber / secondNumber
+const writeInDisplay = (keyContent) => {
+    displayContent.textContent += keyContent
 }
 
+const deleteLastChar = () => {
+    displayContent.textContent = displayContent.textContent.slice(0, -1)
+}
 
+const clearDisplay = () => {
+    displayContent.textContent = ''
+}
+
+const calculate = () => {
+    displayContent.textContent = eval(displayContent.textContent)
+}
 
 // Event Listeners
-
 
 calculator.addEventListener('click', e => {
     const key = e.target
     const action = key.dataset.action
     const keyContent = key.textContent
-    const displayedNum = display.textContent
-
-    if (!action) {
-        console.log('number key!')
+    
+    if (action === 'number') {
+        writeInDisplay(keyContent)
     }
-
+    
     if (
         action === 'add' ||
         action === 'subtract' ||
         action === 'multiply' ||
         action === 'divide'
-    ) {
-        console.log('operator key!')
+        ) {
+            writeInDisplay(keyContent)
     }
 
-    if (action === 'decimal') {
-        console.log('decimal key!')
+    if (action === 'delete') {
+        deleteLastChar()
     }
 
-    if (action === 'clear') {
-        console.log('clear key!')
+    if (action === 'reset') {
+        clearDisplay()
     }
 
     if (action === 'calculate') {
-        console.log('equal key!')
+        calculate()
     }
 })
